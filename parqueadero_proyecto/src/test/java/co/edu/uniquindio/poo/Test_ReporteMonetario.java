9c95d8e42fdfe3a7827201957998dd80ce0d395e
AnduellgDeveloper <anduellgames@gmail.com>
2024-05-21 23:45:42 -0500

    Documentacion explicita de la clase Test_ReporteMonetario

--- parqueadero_proyecto/src/test/java/co/edu/uniquindio/poo/Test_ReporteMonetario.java
+++ parqueadero_proyecto/src/test/java/co/edu/uniquindio/poo/Test_ReporteMonetario.java
@@ -13,6 +13,7 @@ public class Test_ReporteMonetario {
 
     private static final Logger LOG = Logger.getLogger(Test_ReporteMonetario.class.getName());
     
+    // Prueba simple para verificar si la prueba se ejecuta correctamente
     @Test
     public void shouldAnswerWithTrue() {
         LOG.info("Iniciado test shouldAnswerWithTrue");
@@ -20,24 +21,29 @@ public class Test_ReporteMonetario {
         LOG.info("Finalizando test shouldAnswerWithTrue");
     }
 
+    // Prueba para verificar el registro diario de ingresos con datos de vehículos
     @Test
     public void coleccionDiariaConDatos() {
         Parqueadero parqueadero = new Parqueadero(0, 0, 0, 0, 0);
         LOG.info("Inicio prueba de coleccionDiariaConDatos");
         
+        // Crear instancias de vehículos con datos de entrada y salida
         Vehiculo vehiculo1 = new Moto("123", "A", "Juan", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45), 80, TipoMoto.CLASICA);
         Vehiculo vehiculo2 = new Carro("345", "B", "Felipe", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45));
         Vehiculo vehiculo3 = new Moto("567", "C", "Julian", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45), 100, TipoMoto.HIBRIDA);
         Vehiculo vehiculo4 = new Carro("789", "D", "Pepe", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45));
 
+        // Calcular costos de estacionamiento para cada vehículo
         double dinero1 = parqueadero.calcularCosto(vehiculo1);
         double dinero2 = parqueadero.calcularCosto(vehiculo2);
         double dinero3 = parqueadero.calcularCosto(vehiculo3);
         double dinero4 = parqueadero.calcularCosto(vehiculo4);
 
+        // Lista esperada de ingresos diarios
         List<Double> vehiculosEsperados = List.of(dinero1, dinero2, dinero3, dinero4);
         Vehiculo[] vehiculos = {vehiculo1, vehiculo2, vehiculo3, vehiculo4};
 
+        // Registrar ingresos diarios y comparar con la lista esperada
         List<Double> vehiculosLista = ReporteMonetario.registrarDineroDiario(vehiculos, parqueadero);
 
         assertEquals(vehiculosEsperados, vehiculosLista);
@@ -45,6 +51,7 @@ public class Test_ReporteMonetario {
         LOG.info("Fin prueba de coleccionDiariaConDatos");
     }
 
+    // Prueba para calcular el costo diario real y compararlo con el esperado
     @Test
     public void calcularCostoDiarioReal() {
         LOG.info("Inicio prueba de calcularCostoDiarioReal");
@@ -56,9 +63,11 @@ public class Test_ReporteMonetario {
         Vehiculo vehiculo3 = new Moto("333", "C", "Ana", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45), 100, TipoMoto.CLASICA);
         Vehiculo vehiculo4 = new Carro("444", "D", "Andrea", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45));
 
+        // Registrar ingresos diarios para cada vehículo
         List<Vehiculo> vehiculos = List.of(vehiculo1, vehiculo2, vehiculo3, vehiculo4);
         List<Double> ingresosDiarios = ReporteMonetario.registrarDineroDiario(vehiculos.toArray(new Vehiculo[0]), parqueadero);
 
+        // Calcular el costo esperado y real del día y comparar
         double costoEsperado = ingresosDiarios.stream().mapToDouble(Double::doubleValue).sum();
         double costoReal = ReporteMonetario.calcularDineroDiario(ingresosDiarios);
 
@@ -67,24 +76,29 @@ public class Test_ReporteMonetario {
         LOG.info("Fin prueba de calcularCostoDiarioReal");
     }
 
+    // Prueba para verificar el registro mensual de ingresos con datos de vehículos
     @Test
     public void coleccionMensualConDatos() {
         LOG.info("Inicio prueba de coleccionMensualConDatos");
 
         Parqueadero parqueadero = new Parqueadero(0, 0, 0, 0, 0);
+
         Vehiculo vehiculo1 = new Moto("111", "A", "Felipe", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45), 80, TipoMoto.HIBRIDA);
         Vehiculo vehiculo2 = new Carro("222", "B", "Duvan", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45));
         Vehiculo vehiculo3 = new Moto("333", "C", "Ana", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45), 100, TipoMoto.CLASICA);
         Vehiculo vehiculo4 = new Carro("444", "D", "Andrea", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45));
-        
+
+        // Registrar ingresos diarios para dos días diferentes
         List<Vehiculo> vehiculosDia1 = List.of(vehiculo1, vehiculo2);
         List<Vehiculo> vehiculosDia2 = List.of(vehiculo3, vehiculo4);
 
         List<Double> ingresosDia1 = ReporteMonetario.registrarDineroDiario(vehiculosDia1.toArray(new Vehiculo[0]), parqueadero);
         List<Double> ingresosDia2 = ReporteMonetario.registrarDineroDiario(vehiculosDia2.toArray(new Vehiculo[0]), parqueadero);
 
+        // Lista de ingresos diarios para el mes
         List<List<Double>> ingresosDiarios = List.of(ingresosDia1, ingresosDia2);
 
+        // Registrar ingresos mensuales y verificar el tamaño de la lista
         List<Double> ingresosMensuales = ReporteMonetario.registrarDineroMensual(ingresosDiarios);
 
         assertNotNull(ingresosMensuales);
@@ -93,35 +107,47 @@ public class Test_ReporteMonetario {
         LOG.info("Fin prueba de coleccionMensualConDatos");
     }
 
+    // Prueba para calcular el costo mensual real y compararlo con el esperado
     @Test
     public void calcularCostoMensualReal() {
         LOG.info("Inicio prueba de calcularCostoMensualReal");
 
+        // Crear una instancia de Parqueadero con parámetros iniciales en cero
         Parqueadero parqueadero = new Parqueadero(0, 0, 0, 0, 0);
 
+        
         Vehiculo vehiculo1 = new Moto("111", "A", "Felipe", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45), 80, TipoMoto.HIBRIDA);
         Vehiculo vehiculo2 = new Carro("222", "B", "Duvan", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45));
         Vehiculo vehiculo3 = new Moto("333", "C", "Ana", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45), 100, TipoMoto.CLASICA);
         Vehiculo vehiculo4 = new Carro("444", "D", "Andrea", LocalDateTime.of(2024, 5, 14, 12, 45), LocalDateTime.of(2024, 5, 14, 17, 45));
 
+        // Agrupar los vehículos por días
         List<Vehiculo> vehiculosDia1 = List.of(vehiculo1, vehiculo2);
         List<Vehiculo> vehiculosDia2 = List.of(vehiculo3, vehiculo4);
 
+        // Calcular ingresos diarios para cada grupo de vehículos
         List<Double> ingresosDia1 = ReporteMonetario.registrarDineroDiario(vehiculosDia1.toArray(new Vehiculo[0]), parqueadero);
         List<Double> ingresosDia2 = ReporteMonetario.registrarDineroDiario(vehiculosDia2.toArray(new Vehiculo[0]), parqueadero);
 
+        // Agrupar los ingresos diarios en una lista
         List<List<Double>> ingresosDiarios = List.of(ingresosDia1, ingresosDia2);
 
+        // Calcular los ingresos mensuales a partir de los ingresos diarios
         List<Double> ingresosMensuales = ReporteMonetario.registrarDineroMensual(ingresosDiarios);
 
+        // Calcular el costo esperado sumando todos los ingresos diarios
         double costoEsperado = ingresosMensuales.stream().mapToDouble(Double::doubleValue).sum();
+
+        // Calcular el costo real utilizando el método a probar
         double costoReal = ReporteMonetario.calcularDineroMensual(ingresosMensuales);
 
+        // Verificar que el costo esperado y el costo real son iguales
         assertEquals(costoEsperado, costoReal, 0);
 
         LOG.info("Fin prueba de calcularCostoMensualReal");
     }
 
+    // Prueba para verificar la colección de ingresos diarios vacía
     @Test
     public void coleccionDiariaVacia() {
         LOG.info("Inicio prueba de coleccionDiariaVacia");
@@ -134,6 +160,7 @@ public class Test_ReporteMonetario {
         LOG.info("Fin prueba de coleccionDiariaVacia");
     }
 
+    // Prueba para verificar la colección de ingresos mensuales vacía.
     @Test
     public void coleccionMensualVacia() {
         LOG.info("Inicio prueba de coleccionMensualVacia");
